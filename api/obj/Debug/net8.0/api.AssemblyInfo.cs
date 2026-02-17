1c42a484dc6cc1df59167e4652c9ce2e31eb56bd
Mthi123 <s227342720@mandela.ac.za>
2026-02-17 14:03:59 +0200

    readme update

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1b1ebe95b0c1569809ad0ea2639b51dfa8a6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+565537c630ca3609419e892179e63dba0549dfc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
